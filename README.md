5459811b69236b6a3149446675e5460b54825efb
Andre Philippe Figueiredo <figueiredoaphilippe@gmail.com>
2024-03-18 22:38:38 +0000

    Finalizando form validation with zod and react-hook-form

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Ignite Timer
 
 A React + Vite project to apply React important basics concepts.
+
+parei aqui: https://app.rocketseat.com.br/classroom/projeto-02/group/funcionalidades-da-aplicacao/lesson/iniciando-novo-ciclo
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,7 +26,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -36,6 +36,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset() // volta os valores do form para o estado inicial no defaultValues
   }
 
   const task = watch('task')
